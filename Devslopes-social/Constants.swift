11dc69b077e8844e4adfdc2a75f18ac24fcfcdf9
HunterHelms <helmsbrodie@gmail.com>
2017-02-21 11:10:58 -0600

    Sign in UI - in Progress

---
+++ Devslopes-social/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constrants.swift
+//  Devslopes-social
+//
+//  Created by Hunter Helms on 2/21/17.
+//  Copyright © 2017 Reasonable group. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
+
+
